065d43bd0aebb8571c9ea9d004d768c2af118dcd
ARIBILGI <ARIBILGI@P1041>
2021-11-21 15:34:28 +0300

    Add project files.

---
+++ Ders01Degiskenler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ders01Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! ");
+        }
+    }
+}
