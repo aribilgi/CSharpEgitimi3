d24987c8686be53d4fed871248b8d94b96da7401
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2021-12-11 19:04:01 +0300

    Kalıtım konusu bitti - 11-12-2021

--- Ders10OzgenlerProperties/Program.cs
+++ Ders10OzgenlerProperties/Program.cs
@@ -35,6 +35,26 @@ namespace Ders10OzgenlerProperties
             Console.WriteLine("Aylin bilgiler ; Doğum yeri :" + Aylin.DogumYeri + ", Yaş = " + Aylin.Yas + ", Maaş : " + Aylin.Maas);
             Console.WriteLine($"Gizem bilgiler; Doğum yeri :{Gizem.DogumYeri}, Yaş = {Gizem.Yas}, Maaş: {Gizem.Maas}");
 
+            User user = new();
+            user.Adi = "Ali"; // bu şekilde user ın property lerine değer gönderirsek set bloğu çalışır
+            user.Soyadi = "Pusat";
+            //user.Email = "info@uygulama.com";
+            //user.Sifre = "123456";
+
+            Console.WriteLine("Kullanıcı bilgileriniz : ");
+            Console.WriteLine($"Adınız : {user.Adi} Soyadınız : {user.Soyadi}");
+            //--------------------
+            Console.WriteLine("E mail Adresinizi Giriniz:");
+            user.Email = Console.ReadLine();
+            Console.WriteLine("Şifrenizi Giriniz");
+            user.Sifre = Console.ReadLine();
+            var giris = user.KullaniciGiris(user.Email, user.Sifre);
+            if (giris) //burada giris değişkeninden true veya false döneceği için if (giris == true) ile aynı anlama gelir
+            {
+                Console.WriteLine("Sisteme giriş başarılı");
+            }
+            else Console.WriteLine("Giriş Başarısız!");
+
         }
     }
 }
