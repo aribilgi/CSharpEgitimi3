7ab28dc8fd135f8ceefd05833b99b5925df5286f
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-01-08 19:01:19 +0300

    Winform5 deyiz - 8-1-22

--- Ders21Generics/Program.cs
+++ Ders21Generics/Program.cs
@@ -82,5 +82,66 @@ namespace Ders21Generics
             throw new NotImplementedException();
         }
     }
+    //Generic kullanarak interface yapısı
+    interface IProductDal2 : IRepository<Product> //IRepository e parametre olarak product classını gönderdik böylece repositorydeki metotlar product için çalışacak
+    {
+        //Product a özel metot imzası ekleyebiliriz buraya
+    }
+    interface ICategoryDal2 : IRepository<Category> //IRepository e parametre olarak Category classını gönderdik böylece repositorydeki metotlar Category için çalışacak
+    {
+    }
+    class ProductDal2 : IProductDal2
+    {
+        public void Add(Product entity)
+        {
+            Console.WriteLine("Ürün eklendi");
+        }
 
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class CategoryDal2 : ICategoryDal2
+    {
+        public void Add(Category entity)
+        {
+            Console.WriteLine("Kategori Eklendi");
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
