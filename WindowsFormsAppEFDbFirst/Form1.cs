4f4854a2047fb750e0f82e7d400a9e6180a0f95e
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-01-29 15:29:44 +0300

    29-1-22 eşitledik

---
+++ WindowsFormsAppEFDbFirst/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEFDbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Entity Framework
+            /*
+             * Entity Framework ORM(Object Relational Mapping) araçlarından biridir.
+             * Entity Framework ile 4 farklı yöntem kullanarak proje geliştirebilirsiniz.
+             * 
+             * Model First
+             * Database First (Var Olan Veritabanını Kullanma)
+             * Code First (Yeni Veritabanı Kod Yazarak)
+             * Code First (Var Olan Veritabanını Kullanma)
+             */
+        }
+    }
+}
