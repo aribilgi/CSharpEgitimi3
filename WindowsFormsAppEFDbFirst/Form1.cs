33d385887edfbc9dc4198d141a975b199de062b6
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-01-29 18:57:41 +0300

    WindowsFormsAppEFCodeFirst e geçtik - 29-1-2022

--- WindowsFormsAppEFDbFirst/Form1.cs
+++ WindowsFormsAppEFDbFirst/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Migrations;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,6 +31,60 @@ namespace WindowsFormsAppEFDbFirst
              * Code First (Var Olan Veritabanını Kullanma)
              */
             dataGridView1.DataSource = UrunYonetimi.Products.ToList();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            UrunYonetimi.Products.Add(new Products
+            {
+                StokMiktari = Convert.ToInt32(txtStokMiktari.Text),
+                UrunAdi = txtUrunAdi.Text,
+                UrunFiyati = Convert.ToDecimal(txtUrunFiyati.Text)
+            });
+            var eklendi = UrunYonetimi.SaveChanges();
+            if (eklendi > 0)
+            {
+                dataGridView1.DataSource = UrunYonetimi.Products.ToList();
+                MessageBox.Show("Kayıt Eklendi!");
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtUrunAdi.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtUrunFiyati.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtStokMiktari.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            UrunYonetimi.Products.AddOrUpdate(new Products
+            {
+                Id = (int)dataGridView1.CurrentRow.Cells[0].Value,
+                StokMiktari = Convert.ToInt32(txtStokMiktari.Text),
+                UrunAdi = txtUrunAdi.Text,
+                UrunFiyati = Convert.ToDecimal(txtUrunFiyati.Text)
+            });
+            var eklendi = UrunYonetimi.SaveChanges();
+            if (eklendi > 0)
+            {
+                dataGridView1.DataSource = UrunYonetimi.Products.ToList();
+                MessageBox.Show("Kayıt Güncellendi!");
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int kayitId = (int)dataGridView1.CurrentRow.Cells[0].Value;
+
+            var silinecekKayit = UrunYonetimi.Products.Find(kayitId); //Find metodu ef de kayıt bulmamızı sağlar
+            UrunYonetimi.Products.Remove(silinecekKayit);
+            var sonuc = UrunYonetimi.SaveChanges();
+            if (sonuc > 0)
+            {
+                dataGridView1.DataSource = UrunYonetimi.Products.ToList();
+                MessageBox.Show("Kayıt Silindi!");
+            }
         }
     }
 }
