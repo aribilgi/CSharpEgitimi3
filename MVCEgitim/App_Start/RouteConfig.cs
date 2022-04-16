7c959a45a3ee18ecc2c1482d82b2a732d9c4e044
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-04-16 18:01:20 +0300

    16-04-2022 Mvc02HtmlHelpers

--- MVCEgitim/App_Start/RouteConfig.cs
+++ MVCEgitim/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCEgitim
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}", // url yapısı tarayıcı adres çubuğu üzerinden uygulamamıza yapılan isteği kontrol eder. controller a adres çubuğundaki ilk / a kadar yazılan isim yerleştirilir. /{action} kısmı ise bu controllerdaki hangi action ın çalıştırılacağını belirler. /{id} ise bu sayfada id ile bir filtreleme yapılmak istenirse kullanılır ve id zorunlu değildir.
-                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional } // defaults kısmı eğer uygulamaya adres çubuğundan controller ve action adı gelmezse varsayılan olarak hangi controller ve action un çalıştırılacağını belirler
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional } // defaults kısmı eğer uygulamaya adres çubuğundan controller ve action adı gelmezse varsayılan olarak hangi controller ve action un çalıştırılacağını belirler
             );
         }
     }
---
+++ MVCEgitim/Controllers/Mvc01RazorSyntaxController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEgitim.Controllers
+{
+    public class Mvc01RazorSyntaxController : Controller
+    {
+        // Mvc de kendi controller ımızı oluşturmak için Controllers klasörüne sağ tıklayıp add menüsünden Controller a basıyoruz. Açılan pencerede empty i seçip ok ile devam ediyoruz. Açılan pencerede sonu controller ile biten dosyaya bir isim verip ok diyoruz.
+
+        // Controller oluşturduğumuzda Mvc01RazorSyntax yani controller ile aynı isimde Views klasöründe bir klasör oluşur.
+
+        // GET: Mvc01RazorSyntax
+        public ActionResult Index() // Mvc de View sayfası otomatik oluşmaz! Bizim oluşturmamız gerekir. Bir view oluşturmak için Action adına(Index) sağ  tıklayıp add view diyerek açılan pencereden ayarlarını belirleyip view oluştururuz.
+        {
+            return View();
+        }
+    }
+}
