aac1986de724a113193321f55aff4d305d6ddd15
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-04-16 16:13:53 +0300

    16-4-22 Nht

---
+++ MVCEgitim/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVCEgitim
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}", // url yapısı tarayıcı adres çubuğu üzerinden uygulamamıza yapılan isteği kontrol eder. controller a adres çubuğundaki ilk / a kadar yazılan isim yerleştirilir. /{action} kısmı ise bu controllerdaki hangi action ın çalıştırılacağını belirler. /{id} ise bu sayfada id ile bir filtreleme yapılmak istenirse kullanılır ve id zorunlu değildir.
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional } // defaults kısmı eğer uygulamaya adres çubuğundan controller ve action adı gelmezse varsayılan olarak hangi controller ve action un çalıştırılacağını belirler
+            );
+        }
+    }
+}
